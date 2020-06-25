696bed4c3e6da3f3580015f7d782c5829f3cab8a
Rodrigo Ferreira Brandão <rbrandao@farma.local>
2020-06-25 10:24:34 -0300

    Sexto Commit - Responsividades

--- script/scripts.js
+++ script/scripts.js
@@ -1,5 +1,12 @@
 const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card');
+const bodyGit = document.querySelectorAll('.modal-content');
+
+
+const github = fetch("https://raw.githubusercontent.com/sirwhod/Sobre/master/README.md").then(value => {
+        bodyGit.innerHTML += value
+}
+)
 
 for (let card of cards) {
     card.addEventListener("click", function(){
