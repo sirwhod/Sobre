7f27376191a36de23dae92ca8f3af8bfabb722ab
sirwhod <rodrigo.brandao98@gmail.com>
2020-06-28 17:46:34 -0300

    ajuste links pag de contato

--- script/scripts.js
+++ script/scripts.js
@@ -4,10 +4,17 @@ const cards = document.querySelectorAll('.card');
 
 for (let card of cards) {
     card.addEventListener("click", function(){
+        const figmaID = card.getAttribute("id");
+        //const nomeProjeto = card.getElementsByTagName("p");
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector("iframe").src = `https://www.figma.com/embed?embed_host=share&url=${figmaID}chrome=DOCUMENTATION`
+        //modalOverlay.querySelector("h1").innerHTML(`${nomeProjeto}`)
+        //console.log(nomeProjeto);
     })
 }
 
 document.querySelector(".close-modal").addEventListener("click",function(){
     modalOverlay.classList.remove("active")
 })
+
+ 
